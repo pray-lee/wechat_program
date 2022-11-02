00d52b26c9e28fc8fb5358dad6de27e874174d1c
pray-lee <praycis@gmail.com>
2022-11-02 14:38:11 +0800

    update

--- pages/oaList/index.js
+++ pages/oaList/index.js
@@ -50,9 +50,9 @@ Page({
     },
     getOaList() {
         const url = this.data.undo ?
-            app.globalData.url + 'oaTaskController.do?todoDatagrid&field=id,applicationAmount,accountbookId,billType,billCode,taskName,billId,createDate,processInstanceId,remark,status'
+            app.globalData.url + 'oaTaskController.do?todoDatagrid&field=id,accountbookId,accountbookName,billType,billCode,billId,submitterDepartmentId,submitterDepartmentName,submitterName,createDate,billAmount,taskEndTime,taskEndTime_begin,taskEndTime_end,activeUserNames,taskName,'
             :
-            app.globalData.url + 'oaTaskController.do?finishDatagrid&field=id,applicationAmount,accountbookId,billType,billCode,taskName,billId,createDate,processInstanceId,remark,status'
+            app.globalData.url + 'oaTaskController.do?finishDatagrid&field=id,accountbookId,accountbookName,billType,billCode,billId,submitterDepartmentId,submitterDepartmentName,submitterName,createDate,billAmount,taskEndTime,taskEndTime_begin,taskEndTime_end,activeUserNames,taskName,'
         this.addLoading()
         request({
             hideLoading: this.hideLoading,
@@ -67,7 +67,8 @@ Page({
                                 ...item,
                                 billType: this.data.billType[item.billType],
                                 billName: this.data.billName[item.billType],
-                                billClass: this.data.billType[item.billType].toLowerCase()
+                                billClass: this.data.billType[item.billType].toLowerCase(),
+                                billAmount: formatNumber(Number(item.billAmount).toFixed(2))
                             }
                         })
                     })
